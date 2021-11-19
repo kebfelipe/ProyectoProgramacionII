8150a19e25eb508b77fe257ee1442b3f487bc3ab
Jhonproyec <albertcasti0919@gmail.com>
2021-11-19 16:22:09 -0600

    comentarios de la funcionalidad del codigo

--- src/app/paciente/paciente.component.ts
+++ src/app/paciente/paciente.component.ts
@@ -10,6 +10,7 @@ import { PacientesService } from '../service/servicePacientes/pacientes.service'
 })
 export class PacienteComponent implements OnInit {
 
+  //declaramos las variables de cada input que obtenemos del formulario de registro
   id_paciente: number = 0;
   paciente_nombres: string = "";
   paciente_apellidos: string = "";
@@ -19,6 +20,7 @@ export class PacienteComponent implements OnInit {
   paciente_telefono: number = 0;
   paciente_nuevo: Model_Paciente = new Model_Paciente();
 
+  //declaramos las variables de rutas para tener acceso a las rutas y del service, para tener la información del service
   constructor(
     private route:ActivatedRoute, 
     private router:Router, 
@@ -26,15 +28,16 @@ export class PacienteComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    //Aqui guardamos al paciente si es nuevo, de lo contrario se edita
     this.route.paramMap.subscribe((paramMap:any) => {
       const {params} = paramMap;
       this.id_paciente = params.idpaciente == null?0:params.idpaciente;
-
+      //Esta función se encarga de traer la información del paciente que se va a editar
       if(this.id_paciente != 0){
         this.serviceAppPacienteService.get("pacientes/"+this.id_paciente)
         .subscribe((response: any) => {
           console.log(response);
-          
+          //asignamos a los inputs del formulario la informacion que estamos obteniendo del paciente a editar
           this.paciente_nuevo = response;
 
           this.paciente_nombres = this.paciente_nuevo.nombres!;
@@ -50,10 +53,12 @@ export class PacienteComponent implements OnInit {
     
   }
 
+  //Este boton es para regresar a la pagina donde se muestran todos los pacientes
   regresar() {
     window.history.back();
   }
 
+  //Esta funcíon se encarga de verificar si el paciente que vamos a ingresar es nuevo, o si se va a editar a uno
   esNuevo():boolean {     
     if(this.id_paciente == 0) {
       return true;
@@ -62,6 +67,7 @@ export class PacienteComponent implements OnInit {
     } 
   }
 
+  //guardamos la información del paciente en la base de datos
   registrarPaciente():void {     
     this.paciente_nuevo.idPaciente = this.id_paciente;
     this.paciente_nuevo.nombres = this.paciente_nombres;
@@ -76,6 +82,7 @@ export class PacienteComponent implements OnInit {
     });
   }
   
+  //Modificamos la información del paciente que se está modificando
   modificarPaciente():void {     
     this.paciente_nuevo.idPaciente = this.id_paciente;
     this.paciente_nuevo.nombres = this.paciente_nombres;
