6ab44fb1826e5950b3082ede87208f00811e9f5e
AnthonyGom9 <79477732+AnthonyGom9@users.noreply.github.com>
2021-11-19 01:32:34 -0600

    Crecion de componentes
    Agregue los componentes que utilizare en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MenuComponent } from './menu/menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
+import { ConsultaComponent } from './consulta/consulta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    NgbModule,
+    NgbDropdownModule
   ],
+  entryComponents: [],
   providers: [],
   bootstrap: [AppComponent]
 })
